3f96cd03c2ea150c804a97160d4a50c0f1f6896c
Melanie Archer <job@twobanjos.com>
2018-06-12 13:21:17 -0700

    feat: Semantic elements and ARIA
    * put breadcrumb in <nav>
    * fiddle with styling on breadcrumb, nav
    * add ARIA role to #map
    * add ARIA role to breadcrumb
    
    Resolves: #17

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -160,6 +160,7 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('role', 'listitem');
   breadcrumb.appendChild(li);
 };
 
