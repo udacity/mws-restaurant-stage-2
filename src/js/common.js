74d08b513c1b15868e266bc6aca230808cecab8a
Melanie Archer <job@twobanjos.com>
2018-07-11 15:32:37 -0700

    refactor: remove duplicate functions
    * move restaurant images functin to common file

--- src/js/common.js
+++ src/js/common.js
@@ -12,3 +12,30 @@ const registerServiceWorker = () => {
     });
 };
 
+/** 
+ * Create restaurant image(s).
+ */
+const createRestaurantImages = (restaurant, format) => {
+  /* default image; the largest */
+  const image = document.createElement('img');
+  const picture = document.createElement('picture');
+  image.className = 'restaurant-img';
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', restaurant.name);
+  if (format === 'home') {
+ /* When the viewport permits a grid of several items 
+   placed horizontally, use the *smaller* image
+ */
+ picture.innerHTML = `
+   <source media="(min-width: 585px)" srcset="img/${restaurant.id}-thumb.jpg">
+   <source media="(max-width: 584px") srcset="img/${restaurant.id}-small.jpg">
+ `;
+  } else {
+    picture.innerHTML = `
+    <source media="(max-width: 500px)" srcset="img/${restaurant.id}-thumb.jpg">
+    <source media="(min-width: 501px) and (max-width: 600px") srcset="img/${restaurant.id}-small.jpg">
+  `;
+  }
+ picture.append(image);
+ return picture;
+};
